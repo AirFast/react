6609e28e1e1425514c293f4403024f929b286f27
AirFast <airfast.88@gmail.com>
2021-03-01 16:45:54 +0200

    add project

--- src/redux/state.js
+++ src/redux/state.js
@@ -167,28 +167,12 @@ let state = {
             online: true,
             img: 'https://images.unsplash.com/photo-1610824587411-dfcc85fea47a?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NHx8Z2lybCUyMGZhY2V8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
             name: 'Sarah Jonis',
-            messages: [
-                {
-                    text: 'Hi! How are you?'
-                },
-                {
-                    text: 'Are we going for a walk today?'
-                }
-            ]
         },
         {
             id: 2,
             online: true,
             img: 'https://images.unsplash.com/photo-1529111290557-82f6d5c6cf85?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTJ8fGJsYWNrJTIwbWVuJTIwZmFjZXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
             name: 'David Hellhake',
-            messages: [
-                {
-                    text: 'Cool, bro!'
-                },
-                {
-                    text: 'See you tomorrow. I\'ll pick up the tool...'
-                }
-            ]
         },
         {
             id: 3,
