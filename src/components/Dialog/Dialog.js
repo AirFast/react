9eb305d90d8c9a9f495d809dd5c9febfeeb03652
AirFast <airfast.88@gmail.com>
2021-03-02 22:38:27 +0200

    add project

--- src/components/Dialog/Dialog.js
+++ src/components/Dialog/Dialog.js
@@ -3,14 +3,27 @@ import styles from './Dialog.module.css';
 import DialogItem from './DialogItem/DialogItem';
 
 const Dialog = (props) => {
+    const onChangeHandler = (e) => {
+        let message = e.target.value;
+        console.log(message);
+    }
+
+    const onClickHandler = () => {
+        console.log('click');
+    }
+
     return (
-        <article className={styles.article}>
+        <section className={styles.section}>
             <header className={styles.header}>
                 <h1>{props.dialog.user[0].name}</h1>
                 <h1>{props.auth[0].name}</h1>
             </header>
             {props.dialog.messages && props.dialog.messages.map((message, index) => <DialogItem message={message} key={index}/>)}
-        </article>
+            <footer className={styles.footer}>
+                <textarea className={styles.textarea} onChange={ onChangeHandler }></textarea>
+                <button className={styles.button} onClick={ onClickHandler }>+</button>
+            </footer>
+        </section>
     );
 }
 
