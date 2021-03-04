53103771beb57f77d7a998b00ce2b80c9b9c0026
AirFast <airfast.88@gmail.com>
2021-03-04 11:43:27 +0200

    add react-redux

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import React from 'react';
 import styles from './App.module.css';
 import Dialog from './components/Dialog/Dialog';
 import Sidebar from './components/Sidebar/Sidebar';
 import {Route} from 'react-router-dom';
 
 const App = (props) => {
-    return (
-        <div className={styles.container}>
-            <Sidebar dialogs={props.state.dialogs}/>
-            <div className={styles.content}>
-                {props.state.dialogs.map(dialog => <Route path={'/dialog/' + dialog.id} key={dialog.id} render={() => <Dialog dialog={dialog} auth={props.state.auth} dispatch={props.dispatch}/>}/>)}
-            </div>
+  return (
+      <div className={styles.container}>
+        <Sidebar dialogs={props.state.dialogs}/>
+        <div className={styles.content}>
+          {props.state.dialogs.map(dialog => <Route path={'/dialog/' + dialog.id} key={dialog.id} render={() => <Dialog dialog={dialog} auth={props.state.auth} dispatch={props.dispatch}/>}/>)}
         </div>
-    );
+      </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,18 +4,25 @@ import './index.css';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom';
 import store from './redux/redux-store';
+import reportWebVitals from './reportWebVitals';
 
 const rerenderReactDOM = (state) => {
     ReactDOM.render(
-        <BrowserRouter>
-            <App state={state} dispatch={store.dispatch.bind(store)}/>
-        </BrowserRouter>,
+        <React.StrictMode>
+            <BrowserRouter>
+                <App state={state} dispatch={store.dispatch.bind(store)}/>
+            </BrowserRouter>
+        </React.StrictMode>,
         document.getElementById('root')
     );
-
 }
 
 let state = store.getState();
 
 rerenderReactDOM(state);
 store.subscribe(() => (rerenderReactDOM(state)));
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
